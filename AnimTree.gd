e648ef70cfa7400757b14e11f0d8f0ab26d9d58a
ZeEndy <z33ndy@gmail.com>
2025-05-21 11:05:01 +0300

    I hate this editor with a passion. UI/UX nightmare

--- AnimTree.gd
+++ AnimTree.gd
@@ -6,29 +6,38 @@ var SmoothingMode=true
 @onready var BlendSpace : AnimationNodeBlendSpace2D = tree_root.get_node("GroundedMovement")
 @onready var SmoothingText : Label = get_node("Control/Label")
 
-#func _ready() -> void:
-	#print(get_property_list())
+var AnimFPS=25.0
+var IntCounter=0.0
+var speed_scale = 1.0
+
 
 func _process(delta: float) -> void:
 	var XZSpeed=Vector2(Player.velocity.x,-Player.velocity.z).rotated(-Player.Body.global_rotation.y)
 	
+	if callback_mode_process==ANIMATION_CALLBACK_MODE_PROCESS_MANUAL:
+		IntCounter+=delta
+		if IntCounter>1.0/AnimFPS:
+			call_deferred("advance",IntCounter)
+			IntCounter=0.0
 	
-	var TargetSpeedScale=0.01
 	
 	if Input.is_action_just_pressed("SWITCHMODE"):
 		SmoothingMode=!SmoothingMode
 	SmoothingText.text = "BlendPoint Smoothing: "+str(SmoothingMode)
-	#if SmoothingMode:
-		#BlendSpace.velocity_limit=2.5
-	#else:
-		#BlendSpace.velocity_limit=0.0
+	if SmoothingMode:
+		BlendSpace.use_velocity_limit=true
+	else:
+		BlendSpace.use_velocity_limit=false
 	self["parameters/GroundedMovement/blend_position"]=ExpDecay(self["parameters/GroundedMovement/blend_position"],XZSpeed/Player.WALK_SPEED,20,delta)
 		
-	if RootSpeed>0.0 && self["parameters/SpeedScale/scale"] > 0: # check if really needs to be applied
-		TargetSpeedScale = max(XZSpeed.length(),0.01)/max(RootSpeed,0.01)#get the difference
-	self["parameters/SpeedScale/scale"] = lerp(self["parameters/SpeedScale/scale"],TargetSpeedScale,Engine.get_physics_interpolation_fraction()) # interpolate to compensate for physics tik rate
-	if self["parameters/SpeedScale/scale"]==NAN || self["parameters/SpeedScale/scale"]==INF: # incase it some how devides by zero even if it shouldn't
-		self["parameters/SpeedScale/scale"] = 1.0
+		
+	var TargetSpeedScale=0.01
+	TargetSpeedScale = max(XZSpeed.length(),0.01)/max(RootSpeed,0.01)#get the difference
+	speed_scale = lerp(speed_scale,TargetSpeedScale,Engine.get_physics_interpolation_fraction()) # interpolate to compensate for physics tik rate
+	if speed_scale==NAN || speed_scale==INF: # incase it some how devides by zero even if it shouldn't
+		speed_scale = 1.0
+	for i in 11:
+		self["parameters/GroundedMovement/"+str(i+1)+"/TimeScale/scale"]=speed_scale
 
 static func ExpDecay(a,b,decay : float,dt : float):
 	if typeof(a)==TYPE_BASIS:
